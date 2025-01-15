484c853e3e4ed1d95dad71ce904c9b35541db2e1
dkgrbl <denis6b6f@gmail.com>
2025-01-15 18:21:44 +0100

    Add files via upload

---
+++ msq_funcs.py
@@ -0,0 +1,145 @@
+import numpy as np
+import scipy.linalg as lg
+
+
+##########
+########## Covariance matrix 
+##########
+
+
+def transfrom_covariance_with_U(cov_matrix, U_matrix):
+    N = int(cov_matrix.shape[0]/2)
+    M = U_matrix.shape[0]
+    
+    if N!=U_matrix.shape[1]: 
+        raise ValueError("Covariance_matrix is not compatible with unitary transformation!")        
+
+
+    O = np.zeros((2*M, 2*N))
+    O[:M,:N] = np.real(U_matrix)
+    O[:M,N:] = -np.imag(U_matrix)
+    O[M:,:N] = np.imag(U_matrix)
+    O[M:,N:] = np.real(U_matrix)
+
+    return O @ cov_matrix @ O.T
+
+def check_covariance_matrix(cov_matrix, tol=1e-12):
+    if not np.allclose(cov_matrix, cov_matrix.T): 
+        raise ValueError("Covariance matrix is not symetric!")        
+    
+    M = int(cov_matrix.shape[0]/2)
+    Omega = np.zeros((2*M,2*M))
+    Omega[:M,M:] = np.eye(M)
+    Omega[M:,:M] = -np.eye(M)
+    eigenValues, _ =  make_eigendecomp_h(cov_matrix + 1.j*Omega, order="increasing")
+
+    if (np.min(eigenValues)+tol) < 0: 
+        raise ValueError("Covariance matrix is unphysical: uncertanty principle")        
+
+
+    print("Covariance matrix is correct")
+
+
+##########
+########## Additionals
+##########
+
+def make_eigendecomp_h(matrix, order="decreasing"):
+    eigenValues, eigenVectors = np.linalg.eigh(matrix)
+    if order=="decreasing":
+        idx = np.real(eigenValues).argsort()[::-1] 
+    elif order=="increasing":
+        idx = np.real(eigenValues).argsort() 
+    eigenValues = eigenValues[idx]
+    eigenVectors = eigenVectors[:,idx]
+    return eigenValues, eigenVectors
+
+
+##########
+########## Basis recunstruction
+##########
+ 
+
+def build_MSq_basis(cov_matrix, N=20):
+    '''   
+        For a given cov_matrix, the function computes the first N-modes of the MSq-basis
+
+        input:  cov_matrix -- M x M matrices; N-number of computed modes
+        output: U_matrix with the shape N x M; the rows of matrix U_matrix correspond to the Msq-modes 
+    '''
+
+    M = int(np.shape(cov_matrix)[0]/2)
+    if M < N:
+        N = M
+
+    U_matrix = np.zeros((N, M), dtype=complex)
+    buf_U_matrix   = np.identity(M, dtype=complex)
+
+    for i in range(N):
+        print("Itteration:", i)
+        buf_cov_matrix = transfrom_covariance_with_U(cov_matrix, buf_U_matrix) 
+
+        eigen_vals, eigenVec_COV = make_eigendecomp_h(buf_cov_matrix, order="increasing")
+
+        Leig_T = eigenVec_COV.T[0:(M-i),0:(M-i)]
+        Reig_T = eigenVec_COV.T[0:(M-i),(M-i):]
+
+        psevdo_U_matrix = Reig_T + 1.j * Leig_T 
+        Q, _ = lg.qr(psevdo_U_matrix.T)
+
+        buf_U_matrix = Q.T @ buf_U_matrix
+        U_matrix[i, :] = buf_U_matrix[0, :]
+        buf_U_matrix = buf_U_matrix[1:, :]
+    
+
+    if not np.allclose( U_matrix @ np.conj(U_matrix).T, np.identity(N)):
+        raise ValueError("MSq-modes are not orthogonal!")        
+
+    return U_matrix
+
+
+def build_MSq_basis_swap(cov_matrix, N=20, swap_cutoff=1e-5):
+    '''   
+        For a given cov_matrix, the function computes the first N-modes of the MSq-basis
+        For the itteration, when (1.-eigen_vals[0]) < swap_cutoff, the sorting of eigenvalues is changed to get occupied unsqueezed modes.
+
+        input:  cov_matrix -- M x M matrices; N-number of computed modes
+        output: U_matrix with the shape N x M; the rows of matrix U_matrix correspond to the Msq-modes 
+    '''
+    ORDER = "increasing"
+
+    M = int(np.shape(cov_matrix)[0]/2)
+    if M < N:
+        N = M
+
+    U_matrix = np.zeros((N, M), dtype=complex)
+    buf_U_matrix   = np.identity(M, dtype=complex)
+
+    for i in range(N):
+        print("Itteration:", i)
+        buf_cov_matrix = transfrom_covariance_with_U(cov_matrix, buf_U_matrix) 
+
+        eigen_vals, eigenVec_COV = make_eigendecomp_h(buf_cov_matrix, order=ORDER)
+        print(eigen_vals[0])
+
+        if ( (1.-eigen_vals[0] ) < swap_cutoff and ORDER == "increasing" ):
+            print("SWAP itteration:", i+1)
+            ORDER = "decreasing"
+
+        Leig_T = eigenVec_COV.T[0:(M-i),0:(M-i)]
+        Reig_T = eigenVec_COV.T[0:(M-i),(M-i):]
+
+        psevdo_U_matrix = Reig_T + 1.j * Leig_T 
+        Q, _ = lg.qr(psevdo_U_matrix.T)
+
+        buf_U_matrix = Q.T @ buf_U_matrix
+        U_matrix[i, :] = buf_U_matrix[0, :]
+        buf_U_matrix = buf_U_matrix[1:, :]
+    
+
+    if not np.allclose( U_matrix @ np.conj(U_matrix).T, np.identity(N)):
+        raise ValueError("MSq-modes are not orthogonal!")        
+
+    return U_matrix
+
+
---
+++ run_example.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import msq_funcs
+import plot_funcs
+
+
+import os
+FolderPath = os.path.dirname(os.path.realpath(__file__)) + "/" 
+
+
+
+def run_example():
+ 
+    ####
+    #### Load data
+    ####
+
+    PDC =  np.load(os.path.join(FolderPath,"data_63.npz"))
+    cov_matrix_initial = PDC["cov_matrix"]
+
+    msq_funcs.check_covariance_matrix(cov_matrix_initial)
+
+
+    # ####
+    # #### Compute first N_modes from MSq basis 
+    # ####
+
+    N_modes = 200
+    U_msq = msq_funcs.build_MSq_basis_swap(cov_matrix_initial, N=N_modes)
+    covariance_matrix_msq = msq_funcs.transfrom_covariance_with_U(cov_matrix_initial, U_msq)
+    
+    ####
+    #### Plot covariance matrix, squeezing and 3 first modes
+    ####
+
+    plot_funcs.plot_covariance_matrix_xxpp_log(cov_matrix_initial, title="Covariance, initial")
+    plot_funcs.plot_covariance_matrix_xxpp_log(covariance_matrix_msq, title="Covariance, Msq-basis")
+    plot_funcs.plot_squeezing(covariance_matrix_msq)
+    
+    plot_funcs.plot_mode_profile(U_msq, n=0)
+    plot_funcs.plot_mode_profile(U_msq, n=1)
+    plot_funcs.plot_mode_profile(U_msq, n=2)
+    plot_funcs.plot_mode_profile(U_msq, n=43)
+    # plot_funcs.plot_mode_profile(U_msq, n=42)
+
+ 
+if __name__ == '__main__':
+    run_example()
+    plt.show()
